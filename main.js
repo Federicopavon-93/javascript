df565dd3d77a2163c6ffddb3dc620d31cd6ab641
Federicopavon-93 <159828667+Federicopavon-93@users.noreply.github.com>
2024-04-09 09:07:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// comentario e linea
+
+/* 
+comentarios en varias linkeas
+1
+2
+3
+4
+4
+*/
+
+let nombre = 'Federico Pavón' // en caso de poner let nombre = y no poner nada, estoy declarando una variable //
+let edad = 36 // asigna un valor a la variable // 
+let saludo = '¡Muchas Gracias!'
+nombre = 'Jose Fernandez'
+const FECHANACIMIENTO = '07/12/1993'
+
+let valorUno = 12
+let valorDos = 45
+let valorTres = 10
+let valorCuatro = 32
+
+let resultado = valorUno + valorDos
+
+console.log(nombre)
